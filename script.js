0b2172f3674939eeb9376661ef35eb7c14055161
Nilam Chavan <nilam4444@gmail.com>
2020-12-17 15:34:58 +0530

    added files

---
+++ script.js
@@ -0,0 +1,137 @@
+retrieveItemsFromLS();
+calculateAmountSum();
+       
+document.getElementById('inputname').focus();
+
+document.getElementById('addbtn').addEventListener('click', verifyAndAdd);
+
+function addTableRow(){
+    document.getElementById('table').appendChild(onAddBtnClick());    //adds table row in table element
+
+    document.getElementById('inputname').value = "";
+    document.getElementById('inputdate').value = "";
+    document.getElementById('inputamount').value = "";
+    calculateAmountSum();
+}
+
+        function onAddBtnClick() {
+            let expenseName = document.getElementById('inputname').value;
+            let date = document.getElementById('inputdate').value;
+            let amount = document.getElementById('inputamount').value; 
+            return createTableRow(expenseName, date, amount, true);
+        }
+        
+        function createTableRow(name, date, amount, storeInLS){
+            //creates new tr tag in table element
+            let newTableRow = document.createElement('tr');
+            newTableRow.className = "tablerow"; 
+
+            //creates new td tag in tr 
+            let newTableData1 = document.createElement('td');
+            newTableData1.className = "name";     //newTableData1.classList.add("name");
+            newTableData1.innerText = name;
+     
+            let newTableData2 = document.createElement('td');
+            newTableData2.className = "date";   
+            newTableData2.innerText = date;
+ 
+            let newTableData3 = document.createElement('td');
+            newTableData3.className = "amount";     
+            newTableData3.innerText = amount;
+
+            let newTableData4 = document.createElement('td');
+            let deleteBtn = document.createElement('button');
+            deleteBtn.className = "deletebtn";  
+
+            deleteBtn.id = 'deleteBtn' + document.getElementsByClassName('tablerow').length;;
+
+            newTableData4.appendChild(deleteBtn);
+            deleteBtn.innerText = "Delete";
+            deleteBtn.addEventListener('click', deleteRow);
+
+            newTableRow.appendChild(newTableData1);
+            newTableRow.appendChild(newTableData2);
+            newTableRow.appendChild(newTableData3);
+            newTableRow.appendChild(newTableData4);
+
+            if(storeInLS){         //if storeInLS is true
+                storeItemsInLS(name, date, amount);
+            }
+            return newTableRow;
+        }
+
+        function verifyAndAdd(){
+            let inputName = document.getElementById('inputname').value,
+                inputDate = document.getElementById('inputdate').value,
+                inputAmount = document.getElementById('inputamount').value,
+                errorMsg = document.getElementById('error-msg');
+            if(!inputName || !inputDate || !inputAmount){
+                errorMsg.style.display = "block";
+                document.getElementById('inputname').focus();
+            }
+            else {
+                errorMsg.style.display = "none";
+                document.getElementById('inputname').focus();
+                addTableRow();
+            }
+        }
+        
+        function deleteRow(event){
+            event.target.parentNode.parentNode.remove();    //OR this.parentNode.remove();          //'this' here is delete button element 
+            // console.log(event.target);
+          
+            let items = JSON.parse(localStorage.getItem('items')) || [];
+
+            let indexToDelete = parseInt(event.target.id.replace('deleteBtn', ""));   //means eg. from id deleteBtn0 only 0 will stay 
+            items.splice(indexToDelete, 1);                     //deletes 1 delete button element starting from indexToDelete
+            localStorage.setItem("items", JSON.stringify(items));
+
+            // to reassign ids to delete button element ( because id's sequence will be changed after deleting middle row)
+            document.querySelectorAll("table .tablerow").forEach((row, index) => {
+            row.getElementsByClassName("deletebtn")[0].id = "deleteBtn" + index;
+            });
+            calculateAmountSum();
+        }
+
+        //click add button on keyboard enter
+        // Execute a function when the user releases a key on the keyboard
+        document.getElementById('inputamount').addEventListener("keyup", 
+        function(event) {            //you can also remove event parameter and it works
+            // Number 13 is the "Enter" key on the keyboard
+            if(event.keyCode === 13){                                   
+                // Trigger the add button element with a click
+                verifyAndAdd();
+            }
+        });
+
+        function storeItemsInLS(name, date, amount){
+          
+            let obj = {
+                name: name,
+                date: date,
+                amount: amount
+            }
+            //store row items
+            let items = JSON.parse(localStorage.getItem('items')) || [];
+            items.push(obj);
+            localStorage.setItem("items", JSON.stringify(items));
+        }
+
+        function retrieveItemsFromLS(){
+            let itemsfromLS = JSON.parse(localStorage.getItem('items'));
+            if (itemsfromLS) {
+                itemsfromLS.forEach((obj, index) => {
+                    document.getElementById('table').appendChild(createTableRow(obj.name, obj.date, obj.amount, false));
+                })
+            let totalAmtFromLS = JSON.parse(localStorage.getItem('amountsum'));
+            document.getElementById('totalamount').innerText = totalAmtFromLS;
+            }
+        }
+
+        function calculateAmountSum(){
+            let total = 0;
+            document.querySelectorAll('table .tablerow .amount').forEach(item => {
+            total = total + parseInt(item.textContent);
+            });
+            document.getElementById('totalamount').innerText = total;
+        }
